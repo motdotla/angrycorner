d55c12fcc40e41b2fe4a3097450cdbb0e8206deb
scottmotte <scott@scottmotte.com>
2014-03-02 13:23:19 -0600

    Port to work w/ Heroku

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -6,4 +8,5 @@ def index():
   return "angrycorner. We tell you what to be angry about so you don't have too. Never feel left out again in your anger."
 
 if __name__ == "__main__":
-  app.run()
+  port = int(os.environ.get('PORT', 5000))
+  app.run(host='0.0.0.0', port=port)
