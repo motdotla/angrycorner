b2d80c791229d695c59461e1405d63064a941c23
scottmotte <scott@scottmotte.com>
2014-03-02 19:07:14 -0600

    Does trends

--- angrycorner/controllers/trends.py
+++ angrycorner/controllers/trends.py
@@ -14,9 +14,24 @@ def trends_closest():
   twitter_api_secret  = os.environ.get('TWITTER_API_SECRET')
   api = twitter_api.Api(twitter_api_key, twitter_api_secret)
 
-  latitude  = request.args.get('lat', '37.781157')
-  longitude = request.args.get('long', '-122.400612831116')
+  latitude  = request.args.get('lat', '36.1667')
+  longitude = request.args.get('long', '-86.7878')
 
   result = api.get_trends_closest(latitude, longitude)
+
+  return json.dumps(result)
+
+@app.route('/trends/place')
+def trends_place():
+
+  """ trends/place """
+
+  twitter_api_key     = os.environ.get('TWITTER_API_KEY')
+  twitter_api_secret  = os.environ.get('TWITTER_API_SECRET')
+  api = twitter_api.Api(twitter_api_key, twitter_api_secret)
+
+  woeid  = request.args.get('id', '2457170')
+
+  result = api.get_trends_place(woeid)
 
   return json.dumps(result)
