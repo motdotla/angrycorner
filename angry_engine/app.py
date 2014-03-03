7b4ef04a001c49f797254a736d1c679a5b60e836
Carl Shotwell <carl.shotwell@leandog.com>
2014-03-02 19:49:38 -0500

    updating flask app

--- angry_engine/app.py
+++ angry_engine/app.py
@@ -1,7 +1,19 @@
 from flask import Flask
+import os
 from classifier import AngryClassifier
 
+port = int(os.environ.get('PORT', 5000))
+
 app = Flask(__name__)
+app.run(host='0.0.0.0', port=port, debug=True)
 classifier = AngryClassifier()
+
+
+@app.route("/single")
+def classify_single():
+    return "hey"
+    #status = classifier.classify_tweet("This bus is making me sleepy")
+    #return status
+    
 
 
