4f5ba6e0dfb6a66519c09c2c04f55221e28698c1
scottmotte <scott@scottmotte.com>
2014-03-03 20:30:22 -0600

    Moves angry_engine under libs

--- angry_engine/classifier/demo.py
+++
@@ -1,9 +0,0 @@
-from classifier import AngryClassifier
-
-classifier = AngryClassifier()
-print classifier.classify_tweet("I am happy to use Elance tomorrow!")
-
-
-
-#print classifier.classify_many(["I am angry at this bus trip", 
-#                                 "what is a bus trip anyway?"])
